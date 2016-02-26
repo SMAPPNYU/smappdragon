40f2f071b7676bf51c14d2c20f06e3def45e958b
yvan <yvanscher@gmail.com>
2016-02-26 15:42:57 -0500

    so therotically this should run now.

--- smappdragon/base_collection.py
+++ smappdragon/base_collection.py
@@ -44,39 +44,44 @@ class BaseCollection(object):
 					else:
 						returndict[entity_type][TweetParser.get_entity_field('url', entity)] += 1
 
-			if requested_entities['ngrams']:
-				#each of these being fed to get_cleaned_tokens
-				#is a boolean value stored in the dictionary
-				tokens = TweetParser.tokenize_tweet(tweet)
-				for ngram in requested_entities['ngrams']['ngrams']:
-					#get the ngrams for this particular ngram level 
-					#unigram, bigram, tri gram; so this will get all the
-					#bigrams for example from the tokens.
-					thegrams = zip(*[tokens[i:] for i in range(ngram)])
-					#create a single string with all the ngrams; this 
-					#would combine all the bigrams into a string delimited by ' '
-					strgram = ' '.join(str(thegram) for thegram in thegrams)
-					#add this ngram string to the counter array
-					#this takes that ngram string and stores it
-					#at the index like so 2-grams
-					gramindex = '{}-grams'.format(ngram)
-					returndict[gramindex] = strgram
 		returnstructure = {}
-		for key in returndict:
+		for entity_type in returndict:
 			#if the counter is empty
 			#return empty series
-			if len(returndict[key]) < 1:
-				returnstructure[key] =  pd.Series()
+			if len(returndict[entity_type]) < 1:
+				returnstructure[entity_type] = {}
 			##if it's not empty
 			else:
 				#if the number given
 				#is not empty
-				if requested_entities[key]['number']:
+				if requested_entities[entity_type]['number']:
 					#set names and counts to 
-					names, counts = zip(*returndict[key].most_common(requested_entities[key]['number']))
+					names, counts = zip(*returndict[entity_type].most_common(requested_entities[entity_type]['number']))
 				#if the number requested is empty
 				#returns everything
 				else:
-					names, counts = zip(*returndict[key].items())
-				returnstructure[key] =  pd.Series(counts, index=names)
+					names, counts = zip(*returndict[entity_type].items())
+				# for each entity type produce a dictionary with the name
+				# of a particular entity object and its count
+				returnstructure[entity_type] = {name: count for name, count in zip(names, counts)}
 		return returnstructure
+
+	def get_ngrams(self, tweet, requested_ngrams):
+		returndict = {}
+		#each of these being fed to get_cleaned_tokens
+		#is a boolean value stored in the dictionary
+		tokens = TweetParser.tokenize_tweet(tweet)
+		for ngram in requested_ngrams:
+			#get the ngrams for this particular ngram level 
+			#unigram, bigram, tri gram; so this will get all the
+			#bigrams for example from the tokens.
+			thegrams = zip(*[tokens[i:] for i in range(ngram)])
+			#create a single string with all the ngrams; this 
+			#would combine all the bigrams into a string delimited by ' '
+			strgram = ' '.join(str(thegram) for thegram in thegrams)
+			#add this ngram string to the counter array
+			#this takes that ngram string and stores it
+			#at the index like so 2-grams
+			gramindex = '{}-grams'.format(ngram)
+			returndict[gramindex] = strgram
+		return returndict
