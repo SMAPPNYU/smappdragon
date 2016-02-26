cac5ac604fbe368f4da7639456eb794c67377424
yvan <yvanscher@gmail.com>
2016-02-26 14:46:40 -0500

    replaced verbose methods with simplified code

--- smappdragon/tweet_parser.py
+++ smappdragon/tweet_parser.py
@@ -22,51 +22,11 @@ def get_entity(entity_type, tweet):
         return [entity for entity in tweet["entities"][entity_type]]
     return []
 
-'''
-	returns all tweet hashtags as a list of strings (WITHOUT the '#' char)
-'''
-def get_hashtags(tweet):
-    if contains_hashtag(tweet):
-        return [hashtag["text"] for hashtag in tweet["entities"]["hashtags"]]
-    return []
-
-'''
-	returns a list of the URLs in tweet (will return multiples if multiples exist in tweet)
-'''
-def get_urls(tweet):
-    if contains_url(tweet):
-        return [url["expanded_url"] for url in tweet["entities"]["urls"]]
-    else:
-        return []
-
-'''
-	return all 
-'''
-def get_media(tweet):
-    if contains_media(tweet):
-    	return [media["expanded_url"] for media in tweet["entities"]["media"]]
-    else:
-    	return []
-
-'''
-    takes a native tweet (dict). Returns list of all mentioned users in tuple form 
-    (user id_str, user screen name), or empty list if none.
-'''
-def get_user_mentions(tweet):
-    if not contains_mention(tweet):
-        return []
-    users = []
-    for user_mention in tweet["entities"]["user_mentions"]:
-        users.append(user)
-    return users
-
-def get_symbols(tweet):
-    if not contains_mention(tweet):
-        return []
-    symbols = []
-    for symbol in tweet["entities"]["symbols"]:
-        symbols.append(symbol)
-    return symbols
+def get_entity_field(field, entity):
+	if field in entity:
+		return entity[field]
+	else:
+		return None
 
 '''
 	so this says 'match everything that's not alphanumeric + underscore'
