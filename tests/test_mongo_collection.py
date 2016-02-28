63f3a292ebbcc1db79c76fde3f8e8a8314aa05e0
yvan <yvanscher@gmail.com>
2016-02-28 14:46:22 -0500

    pylint resolved for tests

--- tests/test_mongo_collection.py
+++ tests/test_mongo_collection.py
@@ -1,26 +1,31 @@
 import unittest
-from config import *
+from tests.config import config
 from smappdragon import MongoCollection
 
 class TestMongoCollection(unittest.TestCase):
+	collection = MongoCollection( \
+							config['mongo']['host'], \
+							config['mongo']['port'], \
+							config['mongo']['user'], \
+							config['mongo']['password'], \
+							config['mongo']['database'], \
+							config['mongo']['collection'] \
+							)
+
 	def test_mongo_top_entities_returns_dict(self):
-		collection = MongoCollection(config['mongo']['host'], config['mongo']['port'], config['mongo']['user'], config['mongo']['password'], config['mongo']['database'], config['mongo']['collection'])
-		returndict = collection.top_entities({'hashtags':5})
+		returndict = self.collection.top_entities({'hashtags':5})
 		self.assertTrue(isinstance(returndict, dict))
 
 	def test_mongo_top_entities_returns_hashtags(self):
-		collection = MongoCollection(config['mongo']['host'], config['mongo']['port'], config['mongo']['user'], config['mongo']['password'], config['mongo']['database'], config['mongo']['collection'])
-		returndict = collection.top_entities({'hashtags':5})
+		returndict = self.collection.top_entities({'hashtags':5})
 		self.assertTrue(returndict['hashtags'])
 
 	def test_mongo_top_entities_returns_hashtags_and_media(self):
-		collection = MongoCollection(config['mongo']['host'], config['mongo']['port'], config['mongo']['user'], config['mongo']['password'], config['mongo']['database'], config['mongo']['collection'])
-		returndict = collection.top_entities({'user_mentions':5, 'media':3})
+		returndict = self.collection.top_entities({'user_mentions':5, 'media':3})
 		self.assertTrue(returndict['user_mentions'] and returndict['media'])
 
 	def test_mongo_top_entities_returns_counts(self):
-		collection = MongoCollection(config['mongo']['host'], config['mongo']['port'], config['mongo']['user'], config['mongo']['password'], config['mongo']['database'], config['mongo']['collection'])
-		returndict = collection.top_entities({'urls':5, 'symbols':3})
+		returndict = self.collection.top_entities({'urls':5, 'symbols':3})
 		if len(returndict['urls']) > 0:
 			self.assertTrue(len(returndict['urls']) == 5)
 		if len(returndict['symbols']) > 0:
