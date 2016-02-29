58d364733de284bfa4f071fc6069e875f30cc56e
yvan <yvanscher@gmail.com>
2016-02-29 18:20:48 -0500

    changed spacing

--- tests/test_mongo_collection.py
+++ tests/test_mongo_collection.py
@@ -5,14 +5,14 @@ from smappdragon import MongoCollection
 class TestMongoCollection(unittest.TestCase):
 
 	def test_iterator_returns_tweets(self):
-		collection = MongoCollection(     \
-			config['mongo']['host'],      \
-			config['mongo']['port'],      \
-			config['mongo']['user'],      \
-			config['mongo']['password'],  \
-			config['mongo']['database'],  \
-			config['mongo']['collection'] \
-		)
+        collection = MongoCollection(     \
+            config['mongo']['host'],      \
+            config['mongo']['port'],      \
+            config['mongo']['user'],      \
+            config['mongo']['password'],  \
+            config['mongo']['database'],  \
+            config['mongo']['collection'] \
+        )
 		self.assertTrue(len(list(collection.set_limit(10).get_iterator())) > 0)
 
 if __name__ == '__main__':
