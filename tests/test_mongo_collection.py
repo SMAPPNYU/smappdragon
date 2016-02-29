d58a6c44fbf7bc33a9d9f65241e66e385bb2c733
yvan <yvanscher@gmail.com>
2016-02-29 10:36:18 -0500

    test reformat

--- tests/test_mongo_collection.py
+++ tests/test_mongo_collection.py
@@ -11,49 +11,49 @@ class TestMongoCollection(unittest.TestCase):
 
 	def test_mongo_top_entities_returns_dict(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		returndict = collection.top_entities({'hashtags':5})
 		self.assertTrue(isinstance(returndict, dict))
 
 	def test_mongo_top_entities_returns_hashtags(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		returndict = collection.top_entities({'hashtags':5})
 		self.assertTrue('hashtags' in returndict)
 
 	def test_mongo_top_entities_returns_hashtags_and_media(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		returndict = collection.top_entities({'user_mentions':5, 'media':3})
 		self.assertTrue('user_mentions' in returndict and 'media' in returndict)
 
 	def test_mongo_top_entities_returns_counts(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		returndict = collection.top_entities({'urls':5, 'symbols':3})
 		if len(returndict['urls']) > 0:
 			self.assertTrue(len(returndict['urls']) == 5)
@@ -62,39 +62,39 @@ class TestMongoCollection(unittest.TestCase):
 
 	def test_limit_is_set(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		collection.set_limit(5)
 		self.assertEqual(5, collection.limit)
 		collection.set_limit(0)
 
 	def test_limit_actually_limits(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		collection.get_iterator()
 		count = len([tweet for tweet in collection.set_limit(5).get_iterator()])
 		self.assertEqual(5, count)
 
 	def test_filter_is_set(self):
 		collection = MongoCollection( \
-							config['mongo']['host'], \
-							config['mongo']['port'], \
-							config['mongo']['user'], \
-							config['mongo']['password'], \
-							config['mongo']['database'], \
-							config['mongo']['collection'] \
-							)
+			config['mongo']['host'], \
+			config['mongo']['port'], \
+			config['mongo']['user'], \
+			config['mongo']['password'], \
+			config['mongo']['database'], \
+			config['mongo']['collection'] \
+		)
 		collection.set_filter({'a':'b', 'c':'d', 'e':{ 'f':'g', 'h':'i' }})
 		self.assertEqual(collection.filter, {'a':'b', 'c':'d', 'e':{ 'f':'g', 'h':'i' }})
 
