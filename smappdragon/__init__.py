6d957ab224b30a0f3fe16b7befad14be22c77d9a
yvan <yvanscher@gmail.com>
2016-02-29 14:51:18 -0500

    forgot init files

--- smappdragon/__init__.py
+++ smappdragon/__init__.py
@@ -2,5 +2,6 @@
 module marker for smappdragon
 '''
 
+from smappdragon.collection.bson_collection import BsonCollection
 from smappdragon.collection.mongo_collection import MongoCollection
 from smappdragon.tools.tweet_parser import TweetParser
--- smappdragon/collection/__init__.py
+++ smappdragon/collection/__init__.py
@@ -2,4 +2,5 @@
 module indicator for collection
 '''
 from . import mongo_collection
-__all__ = ['mongo_collection']
+from . import bson_collection
+__all__ = ['mongo_collection', 'bson_collection']
