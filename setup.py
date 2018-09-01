99c89a93e84ed7d70dd93dc98708c4a2f016ac44
yinleon <hello.leonyin@gmail.com>
2018-08-31 23:53:36 -0400

    uploading setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(name='smappdragon',
 	packages=['smappdragon', 'smappdragon.collection', 'smappdragon.tools'],
-	version='0.0.44',
+	version='0.0.47',
 	description='smappdragon is a set of tools for working with twitter data',
-	author='yvan',
-	author_email='yns207@nyu.edu',
+	author='yvan, leon yin',
+	author_email='whereisleon@gmail.com',
 	url='https://github.com/SMAPPNYU/smappdragon',
 	keywords='twitter parse smappdragon',
 	license='MIT',
 	install_requires=[
-	  'pymongo>=3.2.1'
+		'pymongo>=3.2.1'
 	]
 )
